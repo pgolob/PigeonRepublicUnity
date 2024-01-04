c9dfd5b6789094c0c6b89e2d7ad147561ebdde68
Danijel Devetak <gd2458@student.uni-lj.si>
2024-01-04 23:16:12 +0100

    neskončno mesto

---
+++ Assets/Scripts/GroundTile.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundTile : MonoBehaviour
+{
+    GroundSpawner groundSpawner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        groundSpawner = GameObject.FindObjectOfType<GroundSpawner>();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("helou");
+
+        groundSpawner.SpawnTile();
+        Destroy(gameObject, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
