a32eec150253bd063d3e146626ef79787b501504
grturk <gt8052@student.uni-lj.si>
2024-01-13 20:59:57 +0100

    Pohitreno okolje

--- Assets/Scripts/MoveObject.cs
+++ Assets/Scripts/MoveObject.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoveObject : MonoBehaviour
 {
-    public float speed = 10f + (PoopSpawn.poopHitCount)*3;
+    public float speed = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +15,7 @@ public class MoveObject : MonoBehaviour
     void Update()
     {
         //transform.Translate(0,0,Vector3.forward * speed * Time.deltaTime);
-        transform.Translate(-Time.deltaTime*speed, 0, 0);
+        float hits = PoopSpawn.poopHitCount;
+        transform.Translate(-Time.deltaTime*speed - hits * (float)0.02, 0, 0);
     }
 }
